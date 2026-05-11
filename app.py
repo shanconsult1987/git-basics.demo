124ba6b6397d7c906de35197c542fdfd88d268e2
shanconsult1987 <78853500+shanconsult1987@users.noreply.github.com>
2026-05-11 12:31:24 +0530

    Set up Flask app with a home route

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello WORLD" > PR)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Dependabot Demo Running"
+
+if __name__ == '__main__':
+    app.run(debug=True)
